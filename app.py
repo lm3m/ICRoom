816e24376b8cee564d59efc5b78bd0aede82eb84
Luke W. McCullough <lukaswm@gmail.com>
2018-12-02 10:24:33 -0500

    forgot to add in the ref to the topic namespace

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import socket
 from rest_plus import api
 from controllers.user_controller import prefix as users_namespace
+from controllers.topics_controller import prefix as topics_namespace
 from database import redis
 
 app = Flask(__name__)
@@ -24,6 +25,7 @@ def main():
     blueprint = Blueprint('api', __name__, url_prefix='/api')
     api.init_app(blueprint)
     api.add_namespace(users_namespace)
+    api.add_namespace(topics_namespace)
     app.register_blueprint(blueprint)
     app.run(host='0.0.0.0', port=80)
 
